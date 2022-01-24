952d97e5d76361ccd34b0b3c7e6448546ccfb26c
Daniel Campos <danielgcampos.04@gmail.com>
2022-01-24 01:42:01 -0300

    fix:homepage and base change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base:"/country_quiz_Dani7uG"
 })
